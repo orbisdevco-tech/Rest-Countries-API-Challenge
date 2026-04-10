379b5552a216cc7c480073f5fd1f3985f67815fc
Zain-ul-din <78583049+Zain-ul-din@users.noreply.github.com>
2026-04-10 15:02:01 +0500

    feat: implemented dark theme

--- app.js
+++ app.js
@@ -9,12 +9,40 @@ function registerComponents() {
   customElements.define("country-page", CountryPage);
 }
 
-registerComponents();
+function handleThemeToggle() {
+  const themeToggleBtn = document.getElementById("theme-toggle-btn");
+  const htmlRoot = document.querySelector("html");
+
+  const setTheme = (isDark) => {
+    if (isDark) htmlRoot.classList.add("dark");
+    else htmlRoot.classList.remove("dark");
+    themeToggleBtn.querySelector("span").textContent = isDark
+      ? "Light Mode"
+      : "Dark Mode";
+  };
+
+  const toggleTheme = () => {
+    const isDark = localStorage.getItem("theme") === "dark";
+    localStorage.setItem("theme", isDark ? "light" : "dark");
+    setTheme(!isDark);
+  };
+
+  themeToggleBtn.addEventListener("click", () => {
+    toggleTheme();
+  });
+
+  const theme = localStorage.getItem("theme");
+  const isDark = theme === "dark";
+  setTheme(isDark);
+}
 
 window.app = {
   router,
 };
 
 window.addEventListener("DOMContentLoaded", () => {
+  handleThemeToggle();
   router.init();
 });
+
+registerComponents();
